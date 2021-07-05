626e88031a1d8e47e623eb2ecb0f1c9810982a3e
Amauri Oliveira <amauriibate32@hotmail.com>
2021-07-05 14:48:24 -0300

    Template do form

--- Forms/src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ Forms/src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styles: []
+  styles: [],
 })
 export class CadastroComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    let name = new FormControl('');
   }
-
 }
+//07:
