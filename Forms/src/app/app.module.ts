a35bf751edd78db18946357bda04d2a9ef518723
Amauri Oliveira <amauriibate32@hotmail.com>
2021-08-04 14:36:43 -0300

    Routing module changes

--- Forms/src/app/app.module.ts
+++ Forms/src/app/app.module.ts
@@ -1,27 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgBrazil } from 'ng-brazil';
 import { TextMaskModule } from 'angular2-text-mask';
-import { CustomFormsModule } from 'ng2-validation'
+import { CustomFormsModule } from 'ng2-validation';
 
+import { NavigationModule } from './navegacao/navigation.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './navegacao/menu/menu.component';
-import { HomeComponent } from './navegacao/home/home.component';
-import { FooterComponent } from './navegacao/footer/footer.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
-import { rootRouterConfig } from './app.routes';
+
+import { AppRoutingModule } from './app.routes';
 import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
 import { CadastroNovoComponent } from './demos/reactiveForms/cadastro-novo/cadastro-novo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent,
-    HomeComponent,
-    FooterComponent,
     SobreComponent,
     CadastroComponent,
     CadastroNovoComponent,
@@ -30,12 +25,13 @@ import { CadastroNovoComponent } from './demos/reactiveForms/cadastro-novo/cadas
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+    NavigationModule,
     TextMaskModule,
     NgBrazil,
     CustomFormsModule,
-    [RouterModule.forRoot(rootRouterConfig, { useHash: false })],
+    AppRoutingModule,
   ],
   providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- Forms/src/app/app.routes.ts
+++ Forms/src/app/app.routes.ts
@@ -1,13 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './navegacao/home/home.component';
 import { SobreComponent } from './institucional/sobre/sobre.component';
 import { CadastroComponent } from './demos/reactiveForms/cadastro/cadastro.component';
 import { CadastroNovoComponent } from './demos/reactiveForms/cadastro-novo/cadastro-novo.component';
 
-export const rootRouterConfig: Routes = [
+const rootRouterConfig: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'sobre', component: SobreComponent },
     { path: 'cadastro', component: CadastroComponent },
     { path: 'cadastro-novo', component: CadastroNovoComponent },
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(rootRouterConfig)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule { }
